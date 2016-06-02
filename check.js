d4201e4072d108f220402babe26fb4aa3f3e98d7
Jeff Dean <jeff@galvanize.com>
2016-06-02 11:02:07 -0600

    added a quick check script

---
+++ check.js
@@ -0,0 +1,11 @@
+const data = require('./data/grades.json')
+const gradeParser = require('./src/grade_parser')
+let counter = 0
+
+for (var key in data) {
+  if (gradeParser.inDecline(data[key])) {
+    counter++
+  }
+}
+
+console.log(counter);
