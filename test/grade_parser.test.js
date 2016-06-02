c811c611557b405eddd5edbc73c8c7d662e676dc
Jeff Dean <jeff@galvanize.com>
2016-06-02 11:59:07 -0600

    added more thorough tests

--- test/grade_parser.test.js
+++ test/grade_parser.test.js
@@ -30,12 +30,15 @@ describe("gradeParser", function() {
       expect(gradeParser.inDecline([5,5,4,4,3,3,2,2])).to.eq(true);
       expect(gradeParser.inDecline([5,4,4,3,2,2])).to.eq(true);
       expect(gradeParser.inDecline([5,5,4,3,3,3,3,3,2,1])).to.eq(true);
+      expect(gradeParser.inDecline([10, 10, 10, 9, 9, 8, 8, 8, 8, 7])).to.eq(true);
+      expect(gradeParser.inDecline([76,62,7,7,1])).to.eq(true);
     });
 
     it("returns false if there are fewer than 3 downs", function() {
       expect(gradeParser.inDecline([5,4,3])).to.eq(false);
       expect(gradeParser.inDecline([5,5,4,4,3,3])).to.eq(false);
       expect(gradeParser.inDecline([5,5,4,4,5,3,2])).to.eq(false);
+      expect(gradeParser.inDecline([ 71, 4, 27, 21, 69, 62, 65, 64, 38, 35, 41, 19 ])).to.eq(false);
     });
   });
 
