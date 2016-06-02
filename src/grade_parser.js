febc5582e0f24b744a520158127e1a281ed18ffa
Jeff Dean <jeff@galvanize.com>
2016-06-02 10:53:34 -0600

    added solution

--- src/grade_parser.js
+++ src/grade_parser.js
@@ -1,5 +1,24 @@
 exports.getMovements = function (grades) {
+  let result = []
+  for (var i = 1; i < grades.length; i++) {
+    if (grades[i] < grades[i - 1]) {
+      result.push('down')
+    } else if (grades[i] > grades[i - 1]) {
+      result.push('up')
+    } else {
+      result.push('even')
+    }
+  }
+  return result;
 }
 
 exports.inDecline = function (grades) {
+  let movements = this.getMovements(grades)
+  let counter = 0
+  for (var i = movements.length; i--;) {
+    if (movements[i] == 'down') counter++
+    if (movements[i] == 'up') counter = 0
+    if (counter === 3) return true
+  }
+  return false
 }
